76ba63b448f876a7ea634250429bc2a0bdc7f969
dprim7 <daniel.primosch@cern.ch>
2025-08-06 03:25:03 -0700

    added stub for output head class for various MET regression strategies (direct, per particle, per event)

---
+++ src/l1deepmet/models/output_head.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+import tensorflow as tf  # type: ignore
+from typing import Any, Dict, Optional
+
+# TODO: implement px,py multiplication where needed
+class OutputHead(ABC):
+    """
+    Abstract base class for output head strategies.
+    MET can be regressed directly or by estimating per particle or per event weight(s).
+    """
+
+    @abstractmethod
+    def build(self, input_dim: int) -> tf.keras.layers.Layer:
+        pass
+
+    @property 
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+class DirectRegressionHead(OutputHead):
+
+    def __init__(self):
+        self.output_dim = 2
+
+    def build(self, input_dim: int) -> tf.keras.layers.Layer:
+        return tf.keras.layers.Dense(
+            self.output_dim,
+            name='direct_regression_head',
+            name='met',
+        ) #TODO: check activation/initialization needs
+
+    @property
+    def name(self) -> str:
+        return 'direct_regression'
+    
+#TODO: implement
+class ParticleWeightHead(OutputHead):
+    pass
+
+#TODO: implement
+class METWeightHead(OutputHead):
+    pass
